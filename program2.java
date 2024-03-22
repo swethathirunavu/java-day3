64b689fe2ed6ffa736f0ef259de8e9fa98b60c29
Swetha T <145834012+swethathirunavu@users.noreply.github.com>
2024-03-22 12:42:40 +0530

    Create program2.java

---
+++ program2.java
@@ -0,0 +1,7 @@
+public class ageAsString {
+   public static void main(String[]args){
+     int age=25;
+     String ageAsString=Integer.toString(age);
+    System.out.println("Age:"+ ageAsString);
+   }
+}
